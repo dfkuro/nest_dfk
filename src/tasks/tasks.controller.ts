23b82c27711651b0af100bf935d6c6756df7935b
dfkuro <izmirreffi@gmail.com>
2021-07-23 18:05:32 -0500

    First commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Delete,
+  Patch,
+  Query,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get.tasks.filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.taskService.getTasksWithFilters(filterDto);
+    } else {
+      return this.taskService.getAllTasks();
+    }
+  }
+
+  @Get(':id')
+  getTaskById(@Param() params): Task {
+    return this.taskService.getTaskById(params.id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.taskService.createTask(createTaskDto);
+  }
+
+  @Delete(':id')
+  deleteTaskById(@Param('id') id): string {
+    this.taskService.deleteTaskById(id);
+    return `Task ${id} deleted`;
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  ): Task {
+    const { status } = updateTaskStatusDto;
+    return this.taskService.updateTaskStatus(id, status);
+  }
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,100 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get.tasks.filter.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  /**
+   * Returns all the tasks available
+   * @returns {array} - Array of tasks
+   */
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  /**
+   * Search a task by status, title or description
+   * @param {GetTaskFilterDto} filterDto
+   * @returns {array} Tasks
+   */
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter((task) => {
+        if (task.title.includes(search) || task.description.includes(search)) {
+          return true;
+        }
+        return false;
+      });
+    }
+
+    return tasks;
+  }
+
+  /**
+   * Returns the task needed
+   * @param {string} id - Id for the task to return
+   * @returns {Task} Task object
+   */
+  getTaskById(id: string): Task {
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with ID "${id}" not found.`);
+    }
+
+    return found;
+  }
+
+  /**
+   * This function create a task for the project
+   * @param {CreateTaskDTO} Check DTO for Task object
+   * @returns {Task} The object created for the task
+   */
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  /**
+   * Remove a Task
+   * @param {string} id - Id for the task to remove
+   * @returns {Task} Task object
+   */
+  deleteTaskById(id: string): void {
+    const found = this.getTaskById(id);
+    this.tasks.filter((task) => task.id !== found.id);
+  }
+
+  /**
+   * Update info for the task
+   * @param {string} id Task identifier
+   * @param {TaskStatus} status
+   * @returns
+   */
+  updateTaskStatus(id: string, status: TaskStatus) {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+}
